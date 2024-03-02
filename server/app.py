e694240eb287fb39b312029b0fcc794bc0f58198
BookmDan <danoh329@gmail.com>
2024-03-01 16:24:18 -0800

    push changes for render deployment

--- server/app.py
+++ server/app.py
@@ -4,5 +4,10 @@ from flask import jsonify, request, make_response, render_template
 from resources.routes import *
 from models.models import *
 
+@app.route('/')
+@app.route('/<int:id>')
+def index(id=0):
+  return render_template('index.html')
+
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
